6680b26e678589e6f5d359c04d72b7bb645a496f
yfukuda <yfukuda@OSAKA-P-0432.local>
2017-05-25 15:59:53 +0900

    featuresスペックの修正検討
    ・spec/features/summaries/の修正できず

--- spec/features/summaries/summaries_page_spec.rb
+++ spec/features/summaries/summaries_page_spec.rb
@@ -2,107 +2,115 @@ require 'rails_helper'
 
 describe 'Summariesページ', :js => true do
 
-  before do
-    set_omniauth
-    visit user_slack_omniauth_authorize_path
-    visit summaries_path
-  end
-
-  context '正常系(ボタンクリックなどによる外観の変化)' do
+  describe 'まとめを作成' do
 
     before do
-      click_link  I18n.t('user.summaries.tables.create_title')
+      set_omniauth
+      visit user_slack_omniauth_authorize_path
+      click_link I18n.t('user.summaries.title')
+      click_link I18n.t('user.summaries.tables.create')
       fill_in 'summary_title',  with: 'Test Title'
       click_button I18n.t('helpers.submit.create')
     end
+
+    let(:summary_id) { Summary.last.id }
 
     specify '作成したまとめがテーブルに挿入される' do
       expect(page).to have_css('td', text: 'Test Title')
     end
 
     specify "#{I18n.t('commons.add')}リンクが表示される" do
+      binding.pry
       #findに変更しないと@summary.saveされる前に検証してしまう(以下、同じ)
-      expect(find('.table')).to have_link(text: I18n.t('commons.add'), href: new_summary_content_path(Summary.last.id))
+      expect(page).to have_link(text: I18n.t('commons.add'), href: new_summary_content_path(summary_id))
     end
 
     specify "#{I18n.t('commons.delete')}リンクが表示される" do
-      expect(find('.table')).to have_link(text: I18n.t('commons.delete'), href:summary_path(Summary.last.id))
+      expect(page).to have_link(text: I18n.t('commons.delete'), href:summary_path(summary_id))
     end
 
-    #TODO: まだ実装できていない
-    context 'まとめを閲覧する' do
+  end
 
-      before do
-        within('.table') do
-          visit summary_path(Summary.last.id)
-        end
-      end
+  #TODO: まだ実装できていない
+  context 'まとめを閲覧する' do
 
-      specify 'タイトルの表示確認' do
-        expect(find('.col-md-9')).to have_css('h3', text: "#{Summary.last.title}")
-      end
-
-      specify 'プロフィール画像の表示確認' do
-        expect(find('.media-left')).to have_selector('img')
-      end
-
-      specify '投稿者の表示確認' do
-        expect(find('.media-body')).to have_selector('h5')
-      end
-
-      specify '投稿内容の表示確認' do
-        expect(find('.media-body')).to have_selector('p')
-      end
-
-      specify "#{I18n.t('commons.delete')}ボタンの表示確認" do
-        expect(find('.media-right')).to have_selector('a')
-      end
-
-      specify "#{I18n.t('user.summaries.forms.back')}リンクの表示確認" do
-        expect(find('.actions')).to have_link(text: "#{I18n.t('user.summaries.forms.back')}", href:summaries_path)
-      end
-
+    before do
+      set_omniauth
+      visit user_slack_omniauth_authorize_path
+      click_link I18n.t('user.summaries.title')
+      click_link I18n.t('user.summaries.tables.create')
+      fill_in 'summary_title',  with: 'Test Title'
+      click_button I18n.t('helpers.submit.create')
+      click_link 'Test Title'
     end
 
-    context 'まとめを削除する' do
+    let(:summary_title){ Summary.last.title }
 
-      let(:summary_id){ Summary.last.id }
-      let(:content_id){ Summary.last.contents.last.id}
+    specify 'タイトルの表示確認' do
+      expect(page).to have_css('h3', text: summary_title)
+    end
+
+    specify 'プロフィール画像の表示確認' do
+      expect(page).to have_selector('img')
+    end
+
+    specify '投稿者の表示確認' do
+      expect(page).to have_selector('h5')
+    end
+
+    specify '投稿内容の表示確認' do
+      expect(page).to have_selector('p')
+    end
+
+    specify "#{I18n.t('commons.delete')}ボタンの表示確認" do
+      expect(page).to have_selector('a')
+    end
+
+    specify "#{I18n.t('user.summaries.forms.back')}リンクの表示確認" do
+      expect(page).to have_link(text: "#{I18n.t('user.summaries.forms.back')}", href:summaries_path)
+    end
+
+  end
+
+  describe 'まとめを削除する' do
+
+    before do
+      set_omniauth
+      visit user_slack_omniauth_authorize_path
+      click_link I18n.t('user.summaries.title')
+      click_link I18n.t('user.summaries.tables.create')
+      fill_in 'summary_title',  with: 'Test Title'
+      click_button I18n.t('helpers.submit.create')
+      click_link I18n.t('commons.delete')
+    end
+
+    let(:summary_id){ Summary.last.id }
+    let(:content_id){ Summary.last.contents.last.id }
+
+    specify 'コンファームメッセージの表示確認' do
+      expect(page).to have_link(text: "#{I18n.t('commons.delete')}", href:summary_content_path(summary_id, content_id))
+      link = find_link I18n.t('commons.delete')
+      expect(link['data-confirm']).to eq(I18n.t('commons.are_you_sure'))
+    end
+
+    context 'コンファームOKの場合' do
 
       before do
-        within('.table') do
-          visit summary_path(Summary.last.id)
+        page.accept_confirm do
+          click_link I18n.t('commons.delete')
         end
       end
 
-      specify 'コンファームメッセージの表示確認' do
-        expect(find('.media-right')).to have_link(text: "#{I18n.t('commons.delete')}", href:summary_content_path(summary_id, content_id))
-        link = find_link I18n.t('commons.delete')
-        expect(link['data-confirm']).to eq(I18n.t('commons.are_you_sure'))
+      specify 'まとめ一覧のページにリダイレクト' do
+        expect(current_path).to eq(summary_path(summary_id))
       end
 
-      context 'コンファームOKの場合' do
+      specify 'Notificationメッセージの表示確認' do
+        expect(page).to have_content (I18n.t('user.contents.messages.destroy'))
+      end
 
-        before do
-          within(find('.media-right')) do
-            page.accept_confirm do
-              click_link I18n.t('commons.delete')
-            end
-          end
-        end
-
-        specify 'まとめ一覧のページにリダイレクト' do
-          expect(current_path).to eq(summary_path(summary_id))
-        end
-
-        specify 'Notificationメッセージの表示確認' do
-          expect(page).to have_content (I18n.t('user.contents.messages.destroy'))
-        end
-
-        specify 'テーブルの行にTitleがTest Titleの行を持たないことを確認' do
-          expect(find('h3')).not_to have_css('h3', text: 'Test Title')
-        end
-
+      specify 'テーブルの行にTitleがTest Titleの行を持たないことを確認' do
+        expect(page).not_to have_css('h3', text: 'Test Title')
       end
 
     end
--- spec/support/env.rb
+++ spec/support/env.rb
@@ -6,8 +6,6 @@ Capybara.default_driver = :rack_test
 Capybara.javascript_driver = :webkit
 # 非表示要素がfindやhave_selectorなどにヒットしないようにする
 Capybara.ignore_hidden_elements = true
-# 最大待ち時間を5秒にする
-Capybara.default_max_wait_time = 5
 
 # Capybara::InfiniteRedirectError: redirected more than 5 times, check for infinite redirects. 対策
 # デフォルトのドライバーのfollow_redirectsをfalseに
