83675fc37d1681a36a35fe96b81d4560a3504bf2
yukihirop <te108186@gmail.com>
2017-06-18 17:52:31 +0900

    config/localesファイルの修正

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-namespace = [Rails.application.class.parent_name, Rails.env].join ':'
-redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
-Redis.current = Redis::Namespace.new(namespace, redis: Redis.new(redis_config))
+unless Rails.env.test?
+  namespace = [Rails.application.class.parent_name, Rails.env].join ':'
+  redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
+  Redis.current = Redis::Namespace.new(namespace, redis: Redis.new(redis_config))
+end
