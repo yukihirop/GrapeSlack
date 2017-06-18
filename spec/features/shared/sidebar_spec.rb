0cdfd51a2803aed4b14c781c85789d31cee893f3
yukihirop <te108186@gmail.com>
2017-06-18 17:57:39 +0900

    その他の spec/featuresスペックを追加

--- spec/features/shared/sidebar_spec.rb
+++ spec/features/shared/sidebar_spec.rb
@@ -2,24 +2,23 @@ require 'rails_helper'
 
 describe 'Sidebar' do
 
+  let(:user) { User.last }
+
   before do
     set_omniauth
     visit user_slack_omniauth_authorize_path
   end
 
-  context '正常系(ボタンクリックなどによる外観の変化)' do
+  subject { page }
+
+  context '表示確認' do
 
     # [参考] http://qiita.com/maangie/items/4522fb16a0fd78fd150e
-    specify 'プロフィールリンクの確認' do
-      expect(page).to have_link(text: I18n.t('user.profile.title'),href:user_profile_path)
-    end
-
-    specify 'まとめ一覧リンクの確認' do
-      expect(page).to have_link(text: I18n.t('user.summaries.title'),href:summaries_path)
-    end
-
-    specify '投稿一覧リンクの確認' do
-      expect(page).to have_link(text: I18n.t('user.contents.title'),href:contents_path)
+    it 'リンクの確認' do
+      is_expected.to have_link(text: I18n.t('user.profile.title'),         href:user_profile_path(user.nickname))
+      is_expected.to have_link(text: I18n.t('user.others_summaries.title'),href:user_others_summaries_path(user.nickname))
+      is_expected.to have_link(text: I18n.t('user.summaries.title'),       href:summaries_path(user.nickname))
+      is_expected.to have_link(text: I18n.t('user.contents.title'),        href:contents_path(user.nickname))
     end
 
   end
--- spec/features/users/profile_page_spec.rb
+++ spec/features/users/profile_page_spec.rb
@@ -2,59 +2,32 @@ require 'rails_helper'
 
 describe 'Profileページ' do
 
+  let(:user) { User.last }
+
   before do
     set_omniauth
     visit user_slack_omniauth_authorize_path
   end
 
-  context '正常系(ボタンクリックなどによる外観の変化)' do
+  subject { page }
+
+  context '表示確認' do
 
     before do
       click_link I18n.t('user.profile.title')
     end
 
-    specify 'テーブルのタイトルの確認' do
-      expect(page).to have_content(I18n.t('user.info'))
+    it 'テーブルのヘッダの確認' do
+      is_expected.to have_content(I18n.t('user.info'))
+      is_expected.to have_content(I18n.t('user.profile.tables.rows.items'))
+      is_expected.to have_content(I18n.t('user.profile.tables.rows.contents'))
     end
 
-    context 'テーブルヘッダ' do
-
-      specify 'テーブルのヘッダ：項目の確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.rows.items'))
-      end
-
-      specify 'テーブルのヘッダ：内容の確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.rows.contents'))
-      end
-
-    end
-
-    context 'テーブル列:項目' do
-
-      specify '名前の確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.name'))
-      end
-
-      specify '姓の確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.last_name'))
-      end
-
-      specify '名の確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.first_name'))
-      end
-
-      specify 'メールアドレスの確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.email'))
-      end
-
-      specify 'パスワードの確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.password'))
-      end
-
-      specify 'プロフィール画像の確認' do
-        expect(page).to have_content(I18n.t('user.profile.tables.profile_img_url'))
-      end
-
+    it '名前の確認' do
+      is_expected.to have_content(user.name)
+      is_expected.to have_content(user.last_name)
+      is_expected.to have_content(user.first_name)
+      is_expected.to have_content(user.email)
     end
 
   end
