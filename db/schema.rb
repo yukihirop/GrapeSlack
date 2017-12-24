fdb403d786025df32bb3d3761261f5d75f104623
aimerald <aimerald3@gmail.com>
2017-12-24 20:28:20 +0900

    session管理にActiveRecordを使えるようにする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531031640) do
+ActiveRecord::Schema.define(version: 20171224112638) do
 
   create_table "contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.bigint "summary_id"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20170531031640) do
     t.datetime "updated_at", null: false
     t.string "profile_image_48_url", null: false
     t.index ["summary_id"], name: "index_contents_on_summary_id"
+  end
+
+  create_table "sessions", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "summaries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
