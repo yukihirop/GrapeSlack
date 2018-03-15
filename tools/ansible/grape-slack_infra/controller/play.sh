c64ec1264e7c24658d42deab4ee11456d36378bc
yukihirop <te108186@gmail.com>
2018-03-16 00:06:20 +0900

    Provisioning Grape Slack
    認証情報だけはのぞいている。
    ここらへんのファイルを別途作成する必要がある。
    
    ```
    ```

---
+++ tools/ansible/grape-slack_infra/controller/play.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PLAYBOOK="$1"
+
+if [ -z $PLAYBOOK ]; then
+    echo "You need to pass a playback as argument to this script."
+    exit 1
+fi
+
+export PYTHONPATH=$(pwd)/credentials
+export GCE_INI_PATH=$(pwd)/inventory/gce.ini
+export SSL_CERT_FILE=$(pwd)/credentials/cacert.pem
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+if [ ! -f "$SSL_CERT_FILE" ]; then
+    curl -O https://curl.haxx.se/ca/cacert.pem
+fi
+
+ansible-playbook -v -i inventory/ "$PLAYBOOK"
